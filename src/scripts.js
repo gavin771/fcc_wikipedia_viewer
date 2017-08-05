1ed603eedc6b571e0b376d6ae3ba7e7719470a60
gavin771 <gavin.samuels10@gmail.com>
2017-08-04 22:06:50 -0500

    added funciton to generate random wiki page

--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
+    $('#search').keypress(function (e) {
+        if (e.which == 13) {//Enter key pressed
+            
+        }
+    });
 
-
+    $('#random').click(function (e) {
+        var win = window.open("https://en.wikipedia.org/wiki/Special:Random", '_blank');
+        win.focus();
+    });
 });
